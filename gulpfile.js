690d8e5c269ff7dc0accfc3c4bc7d92cca2ed7aa
Maxwell Stephens <maxwell@whitehatwebdev.com>
2018-01-31 16:49:54 -0500

    added gulp functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var htmlImport = require('gulp-html-import');
+
  
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('.dist/css'));
+});
+
+ 
+gulp.task('import', function () {
+    gulp.src('./views/index.html')
+        .pipe(htmlImport('./views/components/'))
+        .pipe(gulp.dest('dist')); 
 });
  
 gulp.task('sass:watch', function () {
