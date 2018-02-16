cfa599d3a12d33431d3d04159eda6f243f8a5886
Maxwell Stephens <stephensm2011@gmail.com>
2018-02-16 13:18:44 -0500

    added many things

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var htmlImport = require('gulp-html-import');
-
+var minify = require('gulp-minify');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/css'));
 });
 
@@ -13,21 +20,27 @@ gulp.task('sass', function () {
 gulp.task('import', function () {
     gulp.src('./views/index.html')
         .pipe(htmlImport('./views/components/'))
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist')); 
     gulp.src('./views/about_me.html')
         .pipe(htmlImport('./views/components/'))
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist')); 
     gulp.src('./views/about_you.html')
         .pipe(htmlImport('./views/components/'))
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist')); 
     gulp.src('./views/nuts_bolts.html')
         .pipe(htmlImport('./views/components/'))
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist')); 
     gulp.src('./views/therapy.html')
         .pipe(htmlImport('./views/components/'))
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist')); 
     gulp.src('./views/contact.html')
         .pipe(htmlImport('./views/components/'))
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist')); 
 });
  
